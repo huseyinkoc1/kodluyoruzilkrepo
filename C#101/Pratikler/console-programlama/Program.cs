70a3151ccd2952be00043827170e807a1a9f92a5
huseyinkoc1 <huseyin.koc1@ogr.altinbas.edu.tr>
2021-11-19 22:33:32 +0300

    .
    .

---
+++ C#101/Pratikler/console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı Giriniz.");
+            String ad = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz");
+            String soyad = Console.ReadLine();
+            Console.WriteLine("Merhaba "+ad+" "+soyad);
+            Console.Read();
+        }
+    }
+}
---
+++ C#101/Pratikler/degisken/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
